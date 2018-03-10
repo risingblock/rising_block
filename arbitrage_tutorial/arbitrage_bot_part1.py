c975edecfd7b7b45bad0cab7a4446c2626957d9b
Eddie Chung <mrechung@gmail.com>
2018-03-10 15:01:18 -0800

    Fixed misnamed bittrex. Changed to bitfinex.

--- arbitrage_tutorial/arbitrage_bot_part1.py
+++ arbitrage_tutorial/arbitrage_bot_part1.py
@@ -4,39 +4,39 @@ import pandas as pd
 
 
 def initialze(context):
-    context.bittrex = context.exchanges['bitfinex']
+    context.bitfinex = context.exchanges['bitfinex']
     context.poloniex = context.exchanges['poloniex']
 
-    context.bittrex_trading_pair = symbol('eth_btc', context.bittrex.name)
+    context.bitfinex_trading_pair = symbol('eth_btc', context.bitfinex.name)
     context.poloniex_trading_pair = symbol('eth_btc', context.poloniex.name)
 
 
 def handle_data(context, data):
     poloneix_price = data.current(context.poloniex_trading_pair, 'price')
-    bittrex_price = data.current(context.bittrex_trading_pair, 'price')
+    bitfinex_price = data.current(context.bitfinex_trading_pair, 'price')
 
     print('Date: {}'.format(data.current_dt))
     print('Poloniex: {}'.format(poloneix_price))
-    print('Bittrex: {}'.format(bittrex_price))
+    print('bitfinex: {}'.format(bitfinex_price))
 
-    if (poloneix_price > bittrex_price):
-        # Buy bittrex, sell poloneix
-        print('Buy bittrex, selling poloneix')
-        order(asset=context.bittrex_trading_pair,
+    if (poloneix_price > bitfinex_price):
+        # Buy bitfinex, sell poloneix
+        print('Buy bitfinex, selling poloneix')
+        order(asset=context.bitfinex_trading_pair,
               amount=1,
-              limit_price=bittrex_price)
+              limit_price=bitfinex_price)
         order(asset=context.poloniex_trading_pair,
               amount=-1,
               limit_price=poloneix_price)
 
-    elif (bittrex_price > poloneix_price):
-        print('Buy poloneix, selling bittrex')
+    elif (bitfinex_price > poloneix_price):
+        print('Buy poloneix, selling bitfinex')
         order(asset=context.poloniex_trading_pair,
               amount=1,
               limit_price=poloneix_price)
-        order(asset=context.bittrex_trading_pair,
+        order(asset=context.bitfinex_trading_pair,
               amount=-1,
-              limit_price=bittrex_price)
+              limit_price=bitfinex_price)
 
 
 def analyze(context):
